49b617595bcb8bffe9bcd3166dbbe81b974a075b
Ken Moon <kenjmoon@gmail.com>
2022-03-27 00:14:07 -0400

    Darkmode engage, and some styling and text formatting changes

--- App.tsx
+++ App.tsx
@@ -7,10 +7,17 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text>My name is Jerry Smith, and I love sucking big sweaty boners and licking disgusting furry testicle sacks.</Text>
+      <Text style={styles.mainText}>
+        {'My name is Jerry Smith, and I love sucking big sweaty boners and licking disgusting furry testicle sacks.'}
+        {'\n'}
+        {'\n'}
+      </Text>
       <StatusBar style="auto" />
       <Button onPress={() => setCount(count + 1)} title="Click Me!" />
-      <Text>You clicked {count} times</Text>
+      <Text style={styles.buttonText}>
+        {'\n'}
+        {`You clicked ${count} times`}
+      </Text>
     </View>
   );
 }
@@ -18,8 +25,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#333',
     alignItems: 'center',
     justifyContent: 'center'
   },
+  mainText: {
+    fontSize: 24,
+    color: '#fff',
+    fontWeight: 'bold'
+  },
+  buttonText: {
+    color: '#fff'
+  }
 });
